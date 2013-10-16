fc03c75bf48355437f37397a93e021893cded858
thequbit <thequbit@gmail.com>
2013-10-16 19:19:09 -0400

    almost there ...

--- tools/csv2sqlite.py
+++ tools/csv2sqlite.py
@@ -12,33 +12,67 @@ def readfile(infilename):
             r = []
             for col in row:
                 r.append(col)
-        data.append(r)
+            data.append(r)
         i += 1
-    print i
+    #print i
     return data
 
 def readcolumns(data):
     columns = []
+    #print data[1]
     for i in range(0,len(data[0])):
         name = data[0][i]
-        if '"' in data[1][i] or "'" in data[1][i]:
-            isnum = False
-        else:
+        if data[1][i].replace('-','').isdigit():
             isnum = True
+        else:
+            isnum = False
         columns.append((name,isnum))
     return columns
 
-def createdb(columns,outputfile):
+def writedb(columns,data,outputfile):
     con = sqlite3.connect(outputfile)
-    #CREATE TABLE data
-    #cur
+    cur = con.cursor()
+
+    # create the table
+    query = "CREATE TABLE data(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    for column in columns:
+        name,isnum = column
+        t = "TEXT"
+        if isnum:
+            t = "INT"
+        query += "{0} {1},".format(name,t)
+    query += ")"
+    cur.execute(query)
+
+    # load the data
+    query = "INSERT INTO data VALUES("
+    i = 0
+    for row in data:
+        if not i == 0:
+            for col in row:
+                query += "{0},".format(col)
+    query = query[:-1] # last coma
+    query += ")"
+    cur.execute(query)
+    
+    cur.close()
+    con.close()
+
+    return con
 
 def main():
     infilename = "../data/BUDGET-2014-DB-1.csv"
     outfilename = "output.sqlite"
+    
+    print "Reading data ..."
     data = readfile(infilename)
-    print data
+    
+    print "Processing data ..."
     columns = readcolumns(data)
-    print columns
+    
+    print "Writing out data ..."
+    writedb(columns,data,outfilename)
+    
+    print "Done."
 
 main()
